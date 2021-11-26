b7317beb28cbbe21299cd9a384823728cbbd0277
Niko19452 <94481790+Niko19452@users.noreply.github.com>
2021-11-26 21:32:12 +0000

    Data Loaded and checked

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 # Dataset 'All residential properties sold in Ireland from 2010 to May 28th 2021’ downloaded from kaggle.com
 
 import pandas as pd
+import numpy as np
+
 # load the dataset
 data = pd.read_csv("/Users/dominiccasey/PycharmProjects/Source Files/Property_Price_Register_Ireland-28-05-2021.csv")
-# Preview the first 5 lines of the loaded data
-data.head()
 
-print(data)
+# Check the first 5 rows of the dataset
+print (data.head())
+
+
+
+
