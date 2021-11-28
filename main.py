d5ef5e6ff6697d89f805bb9ceb0ba57158fa3389
Niko19452 <94481790+Niko19452@users.noreply.github.com>
2021-11-28 18:09:38 +0000

    Create a dataframe

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # 1) Real-world dataset. 'All residential properties sold in Ireland from 2010 to May 28th 2021’ downloaded csv file from kaggle.com
 
 # import the Pandas liberies as pd
+import plistlib
+
 import pandas as pd
 
 # import NmmPy liberies as np
@@ -12,14 +14,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 2) Importing the data. load the dataset. 'All residential properties sold in Ireland from 2010 to May 28th 2021’ downloaded from kaggle.com
-data = pd.read_csv("/Users/dominiccasey/PycharmProjects/Source Files/Property_Price_Register_Ireland-28-05-2021.csv")
+df = pd.read_csv("/Users/dominiccasey/PycharmProjects/Source Files/Property_Price_Register_Ireland-28-05-2021.csv")
 
 # 3) Analysising the data. Check the first 5 rows of the dataset and view header names
-print (data.head())
-print (data.head(0))
+print (df.head())
+print (df.head(0))
 
 # Postal codes are unique therefore check the postal code to elimate duplicates. True get converted to 1 and False get converted to 0
-data.duplicated('POSTAL_CODE').sum()
+df.duplicated('POSTAL_CODE').sum()
+
+# iterrows and merging
+# define a smaller datafram df1
+df1 = pd.DataFrame(df)
+df1.iloc[0]
+
+# display VAT Excluded purchases for Cork
+df2=(df.loc[df.COUNTY == 'Cork'])
+df3 = (df2.loc[ df.IF_VAT_EXCLUDED==1])
+print (df3)
+
+
+
 
 
 
