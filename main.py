4601a8e335d7cf293cfa2ba9d7a6eabf914e073c
Niko19452 <94481790+Niko19452@users.noreply.github.com>
2021-11-28 12:45:40 +0000

    Check for duplicates in Post Codes

--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 # Student Name: Dominic Casey Student ID: E10617514
 # Dataset 'All residential properties sold in Ireland from 2010 to May 28th 2021’ downloaded from kaggle.com
 
+# import the Pandas liberies as pd
 import pandas as pd
+
+# import NmmPy liberies as np
 import numpy as np
 
 # load the dataset
 data = pd.read_csv("/Users/dominiccasey/PycharmProjects/Source Files/Property_Price_Register_Ireland-28-05-2021.csv")
 
-# Check the first 5 rows of the dataset
+# Check the first 5 rows of the dataset and view header names
 print (data.head())
+print (data.head(0))
 
+# Postal codes are unique therefore check the postal code to elimate duplicates. True get converted to 1 and False get converted to 0
+data.duplicated('POSTAL_CODE').sum()
+
+# it
 
 
 
