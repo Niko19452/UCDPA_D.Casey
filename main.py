97d4507c089f636f6b694616e9fdfaca54e7618f
Niko19452 <94481790+Niko19452@users.noreply.github.com>
2021-11-28 18:58:35 +0000

    Import Geopandas

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ import numpy as np
 # import Matplotlib which is a plotting library for the Python programming language and its numerical mathematics extension NumPy
 import matplotlib.pyplot as plt
 
+# import Geopandas to plot results on the map of Ireland.
+import geopandas as gpd
+
 # 2) Importing the data. load the dataset. 'All residential properties sold in Ireland from 2010 to May 28th 2021’ downloaded from kaggle.com
 df = pd.read_csv("/Users/dominiccasey/PycharmProjects/Source Files/Property_Price_Register_Ireland-28-05-2021.csv")
+df
 
 # 3) Analysising the data. Check the first 5 rows of the dataset and view header names
 print (df.head())
@@ -29,6 +33,7 @@ df1 = pd.DataFrame(df)
 df1.iloc[0]
 
 # display VAT Excluded purchases for Cork
+# loc() is a label based data selecting and includes the last element of range passed to it
 df2=(df.loc[df.COUNTY == 'Cork'])
 df3 = (df2.loc[ df.IF_VAT_EXCLUDED==1])
 print (df3)
